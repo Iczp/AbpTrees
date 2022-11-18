ab2dcf61ea1ef39af79716e301c814bd19083921
zhongpei <1000@intry.cn>
2022-11-18 15:37:03 +0800

    TreeGetListInput

--- src/IczpNet.AbpTrees.Application.Contracts/AbpTrees/Dtos/TreeGetListInput.cs
+++ src/IczpNet.AbpTrees.Application.Contracts/AbpTrees/Dtos/TreeGetListInput.cs
@@ -1,30 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
+using Volo.Abp.Application.Dtos;
 
 namespace IczpNet.AbpTrees.Dtos
 {
-    public class TreeGetListInput: ITreeGetListInput
+    public class TreeGetListInput : PagedAndSortedResultRequestDto, ITreeGetListInput
     {
-        /// <summary>
-        /// 
-        /// </summary>
         [DefaultValue(false)]
         public virtual bool IsEnabledParentId { get; set; }
-        /// <summary>
-        /// 层级
-        /// </summary>
+
         [DefaultValue(null)]
         public virtual int? Depth { get; set; }
-        /// <summary>
-        /// 上级部门
-        /// </summary>
+
         [DefaultValue(null)]
         public virtual Guid? ParentId { get; set; }
-        /// <summary>
-        /// 关键字(支持拼音)
-        /// </summary>
+
         [DefaultValue(null)]
         public virtual string Keyword { get; set; }
     }
--- src/IczpNet.AbpTrees.Application.Contracts/AbpTrees/ITreeGetListInput.cs
+++ src/IczpNet.AbpTrees.Application.Contracts/AbpTrees/ITreeGetListInput.cs
@@ -4,21 +4,12 @@ namespace IczpNet.AbpTrees
 {
     public interface ITreeGetListInput
     {
-        /// <summary>
-        /// 
-        /// </summary>
         bool IsEnabledParentId { get; set; }
-        /// <summary>
-        /// 层级
-        /// </summary>
+
         int? Depth { get; set; }
-        /// <summary>
-        /// 上级部门
-        /// </summary>
+
         Guid? ParentId { get; set; }
-        /// <summary>
-        /// 关键字(支持拼音)
-        /// </summary>
+
         string Keyword { get; set; }
     }
 }
